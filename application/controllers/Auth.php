360adb8f278fbfdb15451d33d88aee8f6f0c05c8
YusrilAgisha12 <yusril.agisha12@gmail.com>
2019-10-23 08:55:48 +0700

    User&Admin

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -11,16 +11,61 @@ class auth extends CI_Controller
     }
    public function index()
    {
-       $data['title'] = 'JP User Registration';
-       $this->load->view('templates/auth_header',$data);
-       $this->load->view('auth/login');
-       $this->load->view('templates/auth_footer');
+       $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+       $this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+       if($this->form_validation->run() == false) {
+           $data['title'] = 'Login Page';
+           $this->load->view('templates/auth_header',$data);
+           $this->load->view('auth/login');
+           $this->load->view('templates/auth_footer');
+       } else{
+           // validasinya success
+           $this->_login();
+       }
     }
     
+
+       private function _login()
+      {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+        // jika usernya ada
+        if($user) {
+        // jika usernya aktif
+            if($user['is_active'] == 1) {
+              // cek password
+              if(password_verify($password, $user['password'])) {
+                $data = [
+                    'email' => $user['email'],
+                    'role_id' => $user['role_id']
+                ];
+                $this->session->set_userdata($data);
+                redirect('user');
+              } else{
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Kata Sandi Anda Salah!</div>');
+                redirect('auth'); 
+              }
+            }else{
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email Anda Belum Di Aktifkan!</div>');
+                redirect('auth');   
+            }
+        } else{
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email Kamu Tidak Terdaftar!</div>');
+            redirect('auth');
+        }
+      }
+
+
     public function registration()
     {
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'is_unique' => 'This email has already registered!'
+        ]);
         $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
             'matches' => 'Password dont macth!',
             'min_length' => 'Password too short!'
@@ -33,7 +78,30 @@ class auth extends CI_Controller
             $this->load->view('auth/registration');
             $this->load->view('templates/auth_footer');
         } else {
-            echo 'data berhasil ditambahkan!';
+            $data = [
+                'name' => htmlspecialchars($this->input->post('name', true)),
+                'email' => htmlspecialchars($this->input->post('email', true)),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 1,
+                'date_created' => time()
+            ];
+
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Selamat! Akun Kamu Berhasil Silahkan Login!</div>');
+            redirect('auth');
         }
    }
+
+
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('role_id');
+
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Anda Telah Keluar!</div>');
+        redirect('auth');
+    }
+
 }
